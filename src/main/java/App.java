c654ca933d602ec9d115be7da1bc1c74ae4978b1
Elodiiie <glx983793727@163.com>
2022-06-23 22:21:47 +0800

    already test success

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -14,7 +16,54 @@ public class App {
 
     public String bestCharge(List<String> inputs) {
         //TODO: write code here
+        int length = inputs.size();
+        String result = "============= Order details =============\n";
+        String salesName = "";
+        List<String> salesList = new ArrayList<>();
+        double originTotal = 0;
+        double salesTotal = 0;
+        for(int i =0;i<length;i++){
+            String id = inputs.get(i).split(" ")[0];
+            int count = Integer.parseInt(inputs.get(i).split(" ")[2]);
+            for (Item item : itemRepository.findAll()) {
+                if (item.getId().equals(id)) {
+                    double singleTotal = item.getPrice()*count;
+                    result = result.concat(MessageFormat.format("{0} x {1} = {2} yuan\n", item.getName(), count,singleTotal));
+                    for(SalesPromotion salesPromotion : salesPromotionRepository.findAll()){
+                        for(String salesid : salesPromotion.getRelatedItems()){
+                            if(salesid.equals(id)){
+                                salesTotal += singleTotal/2;
+                                salesList.add(item.getName());
+                            }
+                        }
+                    }
 
-        return null;
+                    originTotal +=item.getPrice()*count;
+                    break;
+                }
+            }
+        }
+        salesName = salesName.join("£¬",salesList);
+        String deductResult = result.concat("-----------------------------------\n"+"Promotion used:\n" +
+                "Âú30¼õ6 yuan£¬saving 6 yuan\n" +"-----------------------------------\n"
+                +MessageFormat.format("Total£º{0} yuan\n",originTotal-6) + "===================================");
+        String salesResult =result.concat("-----------------------------------\n"+"Promotion used:\n"
+                +MessageFormat.format("Half price for certain dishes '('{0}')'£¬saving {1} yuan\n",salesName,salesTotal)
+                +"-----------------------------------\n"
+                +MessageFormat.format("Total£º{0} yuan\n",originTotal-salesTotal) + "===================================");
+        if(originTotal >=30){
+            if(originTotal-6 <= originTotal-salesTotal){
+                result = deductResult;
+            }else {
+                result = salesResult;
+            }
+        }else if(salesTotal != 0){
+            result = salesResult;
+        }else{
+            result = result.concat("-----------------------------------\n"
+                    +MessageFormat.format("Total£º{0} yuan\n",originTotal) + "===================================");
+        }
+
+        return result;
     }
 }
